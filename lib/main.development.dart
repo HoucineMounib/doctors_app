929c889c97003206ca2f4ad75286de26434a925f
HoucineMounib <mounibhoucine.services@gmail.com>
2024-07-22 13:26:33 +0100

    - added Development & Production flavors for android - added launch.json for flavors config - change And app name

--- lib/main.development.dart
+++ lib/main.development.dart
@@ -2,9 +2,14 @@ import 'package:doctors_app/core/di/dependancy_injection.dart';
 import 'package:doctors_app/core/routing/app_router.dart';
 import 'package:doctors_app/doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
   SetupGetIt();
+
+  /// To fix .sp font error in release Mode
+
+  await ScreenUtil.ensureScreenSize();
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
